96066d6abd332ebaffd76e6a0baec72612044076
Viacheslav Kolmyk <108751046+Slaik1@users.noreply.github.com>
2023-10-21 00:03:52 +0300

    add login component

--- table-football.UI/src/app/app.module.ts
+++ table-football.UI/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
