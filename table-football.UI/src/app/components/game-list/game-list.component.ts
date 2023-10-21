30afede5429135f8c66a02b24e9015100daee0c0
GAR-07 <garmash.aleksandr.74169701@yandex.ru>
2023-10-21 09:37:36 +0300

    Работа с mock.api

--- table-football.UI/src/app/components/game-list/game-list.component.ts
+++ table-football.UI/src/app/components/game-list/game-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { GameModel } from 'src/app/interfaces/game.model';
+import { GameService } from 'src/app/services/game.service';
 
 @Component({
   selector: 'app-game-list',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./game-list.component.scss']
 })
 export class GameListComponent {
+  games: GameModel[] = [];
+  constructor(
+    private gameService: GameService,
+    ) { }
 
+  ngOnInit(): void {
+    this.getGames(1);
+  }
+
+  getGames(pageNum : number) {
+    this.games = [];
+    this.gameService.getGames()
+    .subscribe({
+      next: (response: any) => {
+        if (response) {
+          for (var i = 0; i < response.length; i++) {
+            this.games.push(response[i]);
+          }
+        }
+      console.log(this.games);
+      },
+      error: (response) => console.log(response)
+    });
+    
+  }
 }
