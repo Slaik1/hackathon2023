998c601da0fd219b20f0e04f3a4767e7bb7bd957
R4yya <121268707+R4yya@users.noreply.github.com>
2023-10-21 17:25:10 +0300

    register rest_framework app

--- table_football_django/table_football_django/settings.py
+++ table_football_django/table_football_django/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.postgres',
+    'rest_framework',
     'custom_users',
     'games',
     'game_logs',
