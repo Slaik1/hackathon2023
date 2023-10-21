e8087497efdbac7151ff102898481bd441333454
GAR-07 <garmash.aleksandr.74169701@yandex.ru>
2023-10-21 15:33:00 +0300

    addStartTopList

---
+++ table-football.UI/src/app/services/user.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environment';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  baseApiUrl: string = environment.baseApiUrl + '/account';
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  getTopUsers() {
+      return this.http.get(this.baseApiUrl + '/users');
+  }
+}
