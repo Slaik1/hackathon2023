5b8e5d4d9e47adf456056e71ad9381fd2ee8407b
GAR-07 <garmash.aleksandr.74169701@yandex.ru>
2023-10-22 00:21:26 +0300

    fix userName

--- table-football.UI/src/app/components/home/login/login.component.ts
+++ table-football.UI/src/app/components/home/login/login.component.ts
@@ -11,7 +11,7 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class LoginComponent {
   loginAccountModel: LoginAccountModel = {
-    userName: '',
+    username: '',
     password: '',
   }
   constructor (
--- table-football.UI/src/app/interfaces/loginAccount.model.ts
+++ table-football.UI/src/app/interfaces/loginAccount.model.ts
@@ -1,4 +1,4 @@
 export interface LoginAccountModel {
-    userName: string;
+    username: string;
     password: string;
 }
