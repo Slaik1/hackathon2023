a107e3fe09959965432d45eb586b110ad59310d4
R4yya <121268707+R4yya@users.noreply.github.com>
2023-10-21 15:43:38 +0300

    create models

--- table_football_django/custom_users/models.py
+++ table_football_django/custom_users/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    userpic = models.ImageField(upload_to='userpics/', blank=True, null=True)
+    rating = models.IntegerField(default=0)
+    game_id = models.IntegerField(blank=True, null=True)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
