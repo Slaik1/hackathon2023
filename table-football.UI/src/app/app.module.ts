cf8e0c41176146d6efe9b11a321b6466b4a9b639
GAR-07 <garmash.aleksandr.74169701@yandex.ru>
2023-10-21 23:25:01 +0300

    addTeamComp

--- table-football.UI/src/app/app.module.ts
+++ table-football.UI/src/app/app.module.ts
@@ -21,6 +21,7 @@ import { StatisticComponent } from './components/cabinet/user-panel/statistic/st
 import { BestPlayersComponent } from './components/home/best-players/best-players.component';
 import { CreateGameComponent } from './components/home/create-game/create-game.component';
 import { UserInfoComponent } from './components/home/user-info/user-info.component';
+import { TeamComponent } from './components/cabinet/user-panel/team/team.component';
 
 @NgModule({
   declarations: [
@@ -41,6 +42,7 @@ import { UserInfoComponent } from './components/home/user-info/user-info.compone
     BestPlayersComponent,
     CreateGameComponent,
     UserInfoComponent,
+    TeamComponent,
   ],
   imports: [
     BrowserModule,
