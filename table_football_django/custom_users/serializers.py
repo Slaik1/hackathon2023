f0aa9526c898e0114858d6d30d696b0e1edb3466
R4yya <121268707+R4yya@users.noreply.github.com>
2023-10-22 09:29:45 +0300

    Update serializers.py

--- table_football_django/custom_users/serializers.py
+++ table_football_django/custom_users/serializers.py
@@ -2,10 +2,12 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Profile
 
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'first_name', 'last_name']
+
 
 class ProfileSerializer(serializers.ModelSerializer):
     username = serializers.CharField(source='user.username')
