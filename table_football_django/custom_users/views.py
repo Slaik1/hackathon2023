37135eead9048910464a51029de223c13639eb3f
R4yya <121268707+R4yya@users.noreply.github.com>
2023-10-22 11:52:44 +0300

    setup user controller

--- table_football_django/custom_users/views.py
+++ table_football_django/custom_users/views.py
@@ -1,7 +1,18 @@
-from django.http import JsonResponse
+from django.db.models import F
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 from .models import Profile
 from .serializers import ProfileSerializer
 
 
-def foo(request):
-    pass
+class TopRatedUsers(generics.ListAPIView):
+    permission_classes = [AllowAny]
+
+    queryset = Profile.objects.exclude(user__is_superuser=True).annotate(username=F('user__username'), email=F('user__email')).order_by('-points')[:20]
+    serializer_class = ProfileSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
