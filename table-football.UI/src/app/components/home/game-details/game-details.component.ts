edf8ef926b4f9461e1406bfb923cea791f843247
GAR-07 <garmash.aleksandr.74169701@yandex.ru>
2023-10-22 11:01:51 +0300

    addEditGameGrid

--- table-football.UI/src/app/components/home/game-details/game-details.component.ts
+++ table-football.UI/src/app/components/home/game-details/game-details.component.ts
@@ -9,21 +9,31 @@ import { UserService } from 'src/app/services/user.service';
 })
 export class GameDetailsComponent {
     users: UserModel[] = [];
-    oneTour: number[] = [];
-    twoTour: number[] = [];
-    threeTour: number[] = [];
+    oneTour1: number[] = [];
+    oneTour2: number[] = [];
+    twoTour1: number[] = [];
+    twoTour2: number[] = [];
+    threeTour1: number[] = [];
+    threeTour2: number[] = [];
+    fourTour1: number[] = [];
+    fourTour2: number[] = [];
     start: number = 0
 
     constructor(
       private userService: UserService,
       ) { }
 
-      
-      
       ngOnInit(): void {
-        this.oneTour = Array.from({ length: (32 - this.start) / 4 + 1 }, (_, i) => this.start + i * 4); this.start += 32;
-        this.twoTour = Array.from({ length: (16 - this.start) / 4 + 1 }, (_, i) => this.start + i * 4); this.start += 16;
-        this.threeTour = Array.from({ length: (8 - this.start) / 4 + 1 }, (_, i) => this.start + i * 4); this.start += 8;
+        const range = (start : number, stop : number, step : number) =>
+          Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + i * step);
+        this.oneTour1 = range(0, 31, 4);
+        this.oneTour2 = range(32, 63, 4);
+        this.twoTour1 = range(64, 79, 4);
+        this.twoTour2 = range(80, 95, 4);
+        this.threeTour1 = range(96, 103, 4);
+        this.threeTour2 = range(104, 111, 4);
+        this.fourTour1 = range(112, 115, 4);
+        this.fourTour2 = range(116, 119, 4);
         this.getTopUsers();
       }
   
