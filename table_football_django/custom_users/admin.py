9501e4fa29b054c9829a9de7f5a09b4d7d79feb0
R4yya <121268707+R4yya@users.noreply.github.com>
2023-10-22 03:43:32 +0300

    admin panel refactoring

--- table_football_django/custom_users/admin.py
+++ table_football_django/custom_users/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Profile
+from django.contrib.auth.models import User
 
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username')
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Profile)
